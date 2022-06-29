4ac48f9545b04b22f108abad5e676c834d01f5cf
taufik <taufik@kalsym.com>
2022-06-29 18:23:26 +0800

    bug fix

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -53,7 +53,7 @@ class VoucherController extends Controller
     public function voucherredeem($voucherId){
         $sql="SELECT A.*, B.name, B.isActivated, B.channel, B.phoneNumber, B.email, C.id AS orderId
              FROM customer_voucher A INNER JOIN customer B ON A.customerId=B.id 
-             INNER JOIN `order` C ON A.voucherId=C.voucherId
+             INNER JOIN `order` C ON A.voucherId=C.voucherId AND A.customerId=C.customerId
              WHERE 
             A.voucherId='".$voucherId."' AND isUsed=1";
         $userList = DB::connection('mysql2')->select($sql);
