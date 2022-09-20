6d55d06b5851cdad309965c2e1e529956204a169
taufik <taufik@kalsym.com>
2022-09-20 16:45:13 +0800

    set country at login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,8 +11,14 @@
                 {{ session('status') }}
             </div>
         @endif
-
-        <form method="POST" action="{{ route('login') }}">
+        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script type="text/javascript">
+            function setCountry() {
+                var val = document.getElementById("selectCountry").value;
+                sessionStorage.setItem('SelectedRegion', val);
+            }
+        </script>
+        <form method="POST" action="{{ route('login') }}" onsubmit="setCountry()">
             @csrf
 
 
